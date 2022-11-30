f7aebc8422645bca9213ab136e651c243205327a
Lena-C-Wolos <118314616+Lena-C-Wolos@users.noreply.github.com>
2022-11-30 18:20:21 +0100

    Create show_pixel_distr.m

--- Zettel2/show_pixel_distr.m
+++ Zettel2/show_pixel_distr.m
@@ -1,34 +1,35 @@
 #Task c and d
 
 load("digit3.mat")
-s_D = size(D,1);
-k = 1;
+s_D = size(D,1);	% number of images
+k = 1;			% index of the figures
 m_x = 0:255;
 
-# Interesting Pixel
-Pixel = [155,297,405,604,188, 467,550,633,380,212]; 
+Pixel = [297,155,203,210,217,399,410,595,605]; % 10 interesting pixel
 
 for j = 1:10
-	p = Pixel(j);
+	p = Pixel(j);		
 	M = zeros(1,256);
-	P = zeros(1,784);
-	P(p) = 255;
+	P = zeros(1,784);	
+	P(p) = 255;		% changes the value at the given pixel p to 255
 
 	for im_num= 1: s_D
 	     I = D(im_num,:);
 	     i=I(p)+1;
-	     M(i)=M(i)+1;
+	     M(i)=M(i)+1;	% M(i) counts the number of images with value i at pixel p
 	end
 
-	P = reshape(P, [28,28]); % converts I to size to 28 x 28
+	P = reshape(P, [28,28]); % converts P to size to 28 x 28
 	P=imrotate(P,270);
 	figure(k); imshow(fliplr(P),[]);
 
 	figure(k+1);
 	bar(m_x,M)
-	title('gray value distribution of pixel number 297');
+	t = strcat("gray value distribution of pixel number ", int2str(p));
+	title(t);
 	axis([-5, 260, 0, 400]);
 	xlabel ('grey value');
+	y = strcat("amount of pixels ", int2str(p) , " to have the greyvalue");
 	ylabel('amount of pixels 297 to have the greyvalue');
-k = k+2;
+k = k+2
 end
