a7002d6e0f79879829993c36425207e53a117d4d
Lena-C-Wolos <118314616+Lena-C-Wolos@users.noreply.github.com>
2022-11-30 17:38:04 +0100

    Update show_mean_and_variance.m

--- Zettel2/show_mean_and_variance.m
+++ Zettel2/show_mean_and_variance.m
@@ -3,15 +3,15 @@ load("digit2.mat")
 
 Sum= zeros(1,784);
 s_D= size(D,1);
-for im_num= 1: s_D
-  for i=1:784
+for im_num= 1: s_D           
+  for i=1:784                
      I = D(im_num,:);
-     Sum(i) = Sum(i) + I(i);
+     Sum(i) = Sum(i) + I(i); % adds the value of the pixel i in picture im_num to the sum of values of pixel i
   end
 end
 
-Avg=Sum/s_D;
-I = reshape(Avg, [28,28]); %converts I to size to 28 x 28
+Avg=Sum/s_D;               % devides the sums by the number of images
+I = reshape(Avg, [28,28]); % converts I to size to 28 x 28
 I = imrotate(I,270);
 figure(1), imshow(fliplr(I),[]);
 
@@ -20,12 +20,12 @@ SumDev= zeros(1,784);
 for im_num= 1: s_D
   for i=1:784
      I = D(im_num,:);
-     SumDev(i) = SumDev(i)+((I(i)-Avg(i))^2);
+     SumDev(i) = SumDev(i)+((I(i)-Avg(i))^2); % adds the square of the value of pixel i minus the mean to the sum 
   end
 end
 
-Var=SumDev/s_D;
+Var=SumDev/s_D; % devides the sums by the number of pictures
 
-I = reshape(Var, [28,28]); %converts I	to size to 28 x 28
+I = reshape(Var, [28,28]); % converts I	to size to 28 x 28
 I=imrotate(I,270);
 figure(2), imshow(fliplr(I),[]);
